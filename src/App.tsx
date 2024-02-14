74167ea153dd6610298a0a3048336e40e9c660dd
Dat Pham <datpt@workhouse.me>
2024-02-14 16:15:59 +0700

    fix bug ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from '../src/pages/Home';
+import Accept from './pages/Accept';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" Component={HomePage} />
+        <Route path="/accept" Component={Accept}/>
       </Routes> 
     </Router>
   );
--- src/pages/Accept.tsx
+++ src/pages/Accept.tsx
@@ -2,20 +2,15 @@ import { useState } from "react";
 import Modal from '../components/Modal';
 
 export default function Accept() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-  console.log(isModalOpen)
+  const [isModalOpen, setIsModalOpen] = useState(true);
   const handleCloseModal = () => {
     setIsModalOpen(false);
   }
   return (
-    <div className="fixed w-full">
-      <div className="grid gap-[10px] m-auto place-items-center w-[100vw]">
-        <img alt="bugcat yayyy" className="h-[400px]" src="/bugcat-yay.gif" />
-        Yayyy~
-          <button className="bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-full" onClick={handleOpenModal}>please choose one</button>
+    <div className="flex w-full justify-center items-center">
+      <div className="grid gap-[10px] m-auto place-items-center w-[100vw] text-center">
+        <img alt="bugcat yayyy" className="h-[300px]" src="/bugcat-yay.gif" />
+        Yayyy~ I'm so happy, let's go out!
       </div>
       {isModalOpen && <Modal onClose={handleCloseModal} />}
     </div>
